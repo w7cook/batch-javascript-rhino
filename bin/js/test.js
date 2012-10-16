f743afcb10434d1bfc0446072645c24469f61749
Brian Bales <brianrbales@gmail.com>
2012-10-16 18:48:24 -0500

    Reverting test.js to avoid unimplemented assignment operation in batch.syntax.Eval

--- bin/js/test.js
+++ bin/js/test.js
@@ -1,8 +1,6 @@
 batch (var root in service) {
   console.log("Directory: " + root.getDir().getName());
-  var i = 0;
   for each (var file in root.getDir().listFiles()) {
-    console.log("^ " + i + ":" + file.getName());
-    i = i + 1;
+    console.log("^ " + file.getName());
   }
 }
