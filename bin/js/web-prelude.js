af5d1eceb130c9f74e42baa44df88ae4d71106a8
Brian Bales <brianrbales@gmail.com>
2012-10-31 04:38:56 -0500

    Updated web-prelude.js to allow running async code (though it is actually syncronous atm)

--- bin/js/web-prelude.js
+++ bin/js/web-prelude.js
@@ -14,12 +14,57 @@ window.onload = (function(old_onload) {
       }
     }
 
+    function asyncObject(data) {
+      var callback, waiting_for;
+      function tryCallback() {
+        if (callback && (waiting_for in data)) {
+          var cb = callback;
+          callback = undefined;
+          cb(data[waiting_for]);
+        }
+      }
+      return {
+        returnObject: {
+          asyncForEach: function(loop_var, step_cb, post_cb) {
+            if (callback) {
+              throw new Error('Multiple async-waits not implemented');
+            }
+            waiting_for = loop_var;
+            callback = function(val) {
+              val = val.concat([]).reverse();
+              function next() {
+                if (val.length > 0) {
+                  step_cb(asyncObject(val.pop()).returnObject, next);
+                } else {
+                  post_cb();
+                }
+              }
+              return next();
+            }
+            tryCallback();
+          },
+          get: function(var_name, cb) {
+            if (callback) {
+              throw new Error('Multiple async-waits not implemented');
+            }
+            waiting_for = var_name;
+            callback = cb;
+            tryCallback();
+          }
+        },
+        set: function(var_name, value) {
+          data[var_name] = value;
+          tryCallback();
+        }
+      };
+    }
+
     var websocket = new WebSocket('ws://localhost:9999');
     var callbacks = [];
     websocket.onmessage = function(event) {
       var callback = callbacks.pop();
       if (callback) {
-        callback(parseMsg(event.data));
+        callback(asyncObject(parseMsg(event.data)).returnObject);
       }
     }
     websocket.onerror = function(event) {
