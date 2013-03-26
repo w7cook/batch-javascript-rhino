f2fd3c44b106dd42c36a21c18adb75c49a3a750a
Brian Bales <brianrbales@gmail.com>
2013-03-25 23:12:32 -0500

    Add seperate basic and sql tests

---
+++ test/test-sql.js
@@ -0,0 +1,25 @@
+var page = {
+  putText: function(text) {
+    var div = document.getElementById("root");
+    var elem = document.createElement('div');
+    elem.appendChild(document.createTextNode(text));
+    div.appendChild(elem);
+  }
+};
+window.onload = (function(old_onload) {
+  return function() {
+    if (old_onload) old_onload.apply(window, arguments);
+
+batch function remote isSoldOut(remote product) {
+  return product.UnitsInStock < 1;
+}
+
+batch (var db in __BATCH_SERVICE__) {
+  for each (var product in db.Products) {
+    if (batch isSoldOut(product)) {
+      page.putText(product.ProductName + " is sold out!")
+    }
+  }
+}
+  };
+})(window.onload);
